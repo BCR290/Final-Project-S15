b6986aeab8105d6534e87d941ff0392d9962fc63
TheCmar7 <smithcr@flip3.engr.oregonstate.edu>
2015-08-11 13:54:29 -0700

    stih

--- src/signup.php
+++ src/signup.php
@@ -1,5 +1,7 @@
 <?php
+	include("common.php");
 
+	print_r($_GET);
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -14,27 +16,27 @@
 				<legend>Sign-Up</legend>
 
 				<label for='username' >UserName*:</label>
-				<input type='text' name='username' id='username' maxlength="50" /><br>
+				<input type='text' name='username' id='username' maxlength="50" id="username_create"/><br>
 
 				<label for='password' >Password*:</label>
-				<input type='password' name='password' id='password' maxlength="50" /><br>
+				<input type='password' name='password' id='password' maxlength="50" id="p1" /><br>
 
 				<label for='password' >Re-enter your Password*:</label>
-				<input type='password' name='password' id='Re-password' maxlength="50" /><br>
+				<input type='password' name='password' id='Re-password' maxlength="50" id="p2" /><br>
 
 				
 				<label for='Fname' >Your First Name*: </label>
-				<input type='text' name='Fname' id='Fname' maxlength="50" /><br>
+				<input type='text' name='Fname' id='Fname' maxlength="50" id="firstname" /><br>
 				
 
 				<label for='Lname' >Your Last Name*: </label>
-				<input type='text' name='Lname' id='Lname' maxlength="50" /><br>
+				<input type='text' name='Lname' id='Lname' maxlength="50" id="lastname" /><br>
 				 
 
 				 
 
 
-				<input type='submit' name='submit' value='Sign-Up' />
+				<input type='submit' name='submit' value='Sign-Up' id="signup" />
 			 
 			</fieldset>
 		</form>
