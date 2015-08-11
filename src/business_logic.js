4f6f600ab96b7d08afd4ba9a2692be2f04a77ac4
TheCmar7 <smithcr@flip2.engr.oregonstate.edu>
2015-08-11 12:28:39 -0700

    I got it to be able to check that the user is in the database

--- src/business_logic.js
+++ src/business_logic.js
@@ -4,11 +4,13 @@
 	var xhr; 
 	
 	window.onload = function() {
-		if (window.XMLHttpRequest) { 						// Mozilla, Safari, ...  
-		    xhr = new XMLHttpRequest();  
-		} else if (window.ActiveXObject) { 					// IE 8 and older  
-		    xhr = new ActiveXObject("Microsoft.XMLHTTP");  
-		} 
+		//if (window.XMLHttpRequest) { 						// Mozilla, Safari, ...  
+		     
+		//} else if (window.ActiveXObject) { 					// IE 8 and older  
+		  //  xhr = new ActiveXObject("Microsoft.XMLHTTP");  
+		//} 
+
+		
 
 		var login_button = document.getElementById("login_button");
 		login_button.onclick = login;
@@ -17,16 +19,18 @@
 
 
 	var login = function() {
+		xhr = new XMLHttpRequest(); 
+		
 		var username = document.getElementById("username").value;
 		var password = document.getElementById("password").value;
 
-		var data = "user_name=" + username + "&password=" + password;
+		var data = "user=" + username + "&pass=" + password + "&action=login";
 		console.log(data);
 
-		xhr.open("POST", "common.php?" + data, true);
+		xhr.open("POST", "login.php", true);
 		xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 		xhr.send(data);
-
+		
 	}
 
 
