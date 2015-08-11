ca0d82aeb033c49b61684f58f12e9220f7042020
TheCmar7 <smithcr@flip3.engr.oregonstate.edu>
2015-08-11 13:54:13 -0700

    THis

--- src/login.php
+++ src/login.php
@@ -1,5 +1,41 @@
 <?php
-	include("common.php")
+	include("common.php");
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
+		//print_r($_POST);
+		if($_POST['submit'] == 'Log-In') {
+			$dbc = getdbc();
+			//
+			//echo '1' . $_POST['user'] . ' ';
+			//echo '2' . $_POST['pass'] . ' ';
+
+			$id = $dbc -> prepare("SELECT id FROM USERS_2 WHERE username = ?");
+			$id -> bind_param("s", $user);
+			$user = $_POST['user'];
+			$id -> execute();
+			$result = $id->get_result();
+			$theFoundIds = $result->fetch_array(MYSQLI_ASSOC);
+			$actualId = $theFoundIds["id"];
+			//echo 'id ' . $actualId. ' ';
+
+
+			$pword = $dbc -> prepare("SELECT password FROM USERS_2 WHERE id = ?");
+			$pword -> bind_param("i", $id);
+			$id = $actualId;
+			$pword -> execute();
+			$result = $pword->get_result();
+			$theFoundPWords = $result->fetch_array(MYSQLI_ASSOC);
+			$actualPWord = $theFoundPWords["password"];
+			if($actualPWord == $_POST['pass']) {
+				header('Location: page.php');
+				echo 'login success';
+				echo '3 ' . $actualPWord;
+			} else {
+				$msg = 'Invalid Username and password combination';
+				echo '<script type="text/javascript">alert("' . $msg . '"); </script>';
+			}
+		}
+	} 
 
 ?>
 
