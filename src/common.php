38061222fe3df5d01511de4a29659c78a158ef7e
TheCmar7 <smithcr@flip2.engr.oregonstate.edu>
2015-08-11 12:29:12 -0700

    I made it check the user and password

--- src/common.php
+++ src/common.php
@@ -6,7 +6,7 @@
 		$dbpass = 'NSIq13ND6ShjS9UV';
 		
 		// $mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
-  //   	or die("Error connecting to database server");
+  		//   	or die("Error connecting to database server");
 
 		// mysql_select_db($dbname, $mysql_handle)
 		// 	or die("Error selecting database: $dbname");
@@ -16,38 +16,42 @@
 	}
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
-		$dbc = getdbc();
-		echo '1' . $_POST['user_name'] . ' ';
-		echo '2' . $_POST['password'] . ' ';
-		echo '1' . $_GET['user_name'] . ' ';
-		echo '2' . $_GET['password'] . ' ';
+		//print_r($_POST);
+		if($_POST['submit'] == 'Log-In') {
+			$dbc = getdbc();
+			//
+			//echo '1' . $_POST['user'] . ' ';
+			//echo '2' . $_POST['pass'] . ' ';
 
-		$id = $dbc -> prepare("SELECT id FROM USERS_2 WHERE username = ?");
-		$id -> bind_param("s", $user);
-		$user = $_POST['username'];
-		$id -> execute();
-		$result = $id->get_result();
-		$theFoundIds = $result->fetch_array(MYSQLI_ASSOC);
-		$actualId = $theFoundIds["id"];
-		echo 'id' . $actualId. ' ';
+			$id = $dbc -> prepare("SELECT id FROM USERS_2 WHERE username = ?");
+			$id -> bind_param("s", $user);
+			$user = $_POST['user'];
+			$id -> execute();
+			$result = $id->get_result();
+			$theFoundIds = $result->fetch_array(MYSQLI_ASSOC);
+			$actualId = $theFoundIds["id"];
+			//echo 'id ' . $actualId. ' ';
 
 
-		$pword = $dbc -> prepare("SELECT password FROM USERS_2 WHERE id = ?");
-		$pword -> bind_param("i", $id);
-		$id = $actualId;
-		$pword -> execute();
-		$result = $pword->get_result();
-		$theFoundPWords = $result->fetch_array(MYSQLI_ASSOC);
-		$actualPWord = $theFoundPWords["password"];
-		if($actualPWord == $_POST['password']) {
-			echo 'login success';
-			echo '3' . $actualPWord;
+			$pword = $dbc -> prepare("SELECT password FROM USERS_2 WHERE id = ?");
+			$pword -> bind_param("i", $id);
+			$id = $actualId;
+			$pword -> execute();
+			$result = $pword->get_result();
+			$theFoundPWords = $result->fetch_array(MYSQLI_ASSOC);
+			$actualPWord = $theFoundPWords["password"];
+			if($actualPWord == $_POST['pass']) {
+				header('Location: page.php');
+				echo 'login success';
+				echo '3 ' . $actualPWord;
+			} else {
+				$msg = 'Invalid Username and password combination';
+				echo '<script type="text/javascript">alert("' . $msg . '"); </script>';
+			}
 		}
-	} else {
-		echo "pooooooooooop";
-	}
+	} 
 
 	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-
+		
 	}
 ?>
