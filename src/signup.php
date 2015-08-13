06b42af77dfc6db65ed9de177b776e37618c37a1
TheCmar7 <smithcr@flip2.engr.oregonstate.edu>
2015-08-13 15:20:54 -0700

    WE CAN ADD USERS NOW! :D

--- src/signup.php
+++ src/signup.php
@@ -3,39 +3,58 @@
 
 	// add SQL thing
 	$avaliable = false;
-	$usernameError = $password1Error = $password2Error = $firstnameError = $lastnameError = false;
+	$usernameError = $password1Error = $password2Error = $firstnameError = $lastnameError = $passwordMissMatch = false;
 
-	print_r($_POST);
-
-	if ($_POST["username"] == null) {
-		$usernameError = true;
-	} else {
-		$given_username = $_POST["username"];
-		foreach ($theFoundUsers as $username => $name) {
-			if(strtolower($given_username) === strtolower($name)) {
-				$avaliable = true;
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if ($_POST["username"] == null) {
+			$usernameError = true;
+			echo "1";
+		} else {
+			$given_username = $_POST["username"];
+			foreach ($theFoundUsers as $username => $name) {
+				if(strtolower($given_username) !== strtolower($name)) {
+					$avaliable = true;
+				}
 			}
 		}
-	}
-	if ($_POST["password"] == null) {
-		$passwordError = true;
-	} 
+		if ($_POST["password1"] == null) {
+			$password1Error = true;
+			//echo "3";
+		}  
+		if ($_POST["password2"] == null) {
+			$password2Error = true;
+			//echo "4";
+		} 
+		if (!$password2Error && !$password1Error) {
+			if ($_POST["password2"] != $_POST["password1"]) {
+				$passwordMissMatch = true ;
+				//echo "5";
+			}
+		}
+		if ($_POST["Fname"] == null) {
+			$firstError = true;
+			//echo "6";
+		} 
+		if ($_POST["Lname"] == null) {
+			$lastnameError = true;
+			//echo "7";
+		} 
+		if (!$usernameError && !$password1Error && !$password2 && !$firstnameError && !$lastnameError && !$passwordMissMatch && $avaliable) {
+			// add them to the database and go to there page 
+			//echo "we did it";
+			$addUser = $dbc -> prepare("INSERT INTO USERS_2 (username, password, first_name, last_name, date_joined) VALUES(?, ?, ?, ?, now())");
+			$addUser->bind_param("ssss", $username, $password, $firstname, $lastname);
+			$username  = $_POST["username"];
+			$password  = $_POST["password1"];
+			$firstname = $_POST["Fname"];
+			$lastname  = $_POST["Lname"];
+			$addUser -> execute();
+			header("Location: page.php");
+		}
 
-	if ($_POST["firstname"] == null) {
-		$firstError = true;
-	} 
-	if ($_POST["lastname"] == null) {
-		$lastnameError = true;
-	} 
-	if (!$usernameError && !$passwordError && !$firstnameError && !$lastnameError && $avaliable) {
-		// add them to the database and go to there page 
-		$dbc = getdbc();
-		$addUser -> bind_param("ssss", $username, $password, $lastname, $firstname);
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-		$firstname = $_POST["firstname"];
-		$lastname = $_POST["lastname"];
-		$addUser -> execute();
+		function showError() {
+			echo "<span class=\"error\"> * </span>";
+		}
 	}
 
 
@@ -55,20 +74,52 @@
 
 				<label for='username' ></label>
 				<input type='text' name='username' id='uname' maxlength="50" id="username_create" onkeyup="check_av(this.value)" placeholder="User Name"/>
-				<span id="error"></span><br>
-
+				<span id="avaliable"></span>
+				<?php
+				if ($usernameError) {
+					showError();
+				}
+				if (!$avaliable && $_SERVER["REQUEST_METHOD"] == "POST") {
+					echo "<span class=\"error\">That username is not Avaliable</span>";
+				}
+				?>
+				<br>
 				<label for='password' ></label>
-				<input type='password' name='password1' id='pword' maxlength="50" placeholder="Password"/><br>
-
+				<input type='password' name='password1' id='pword' maxlength="50" placeholder="Password"/>
+				<?php
+				if ($password1Error) {
+					showError();
+				}
+				?>
+				<br>
 				<label for='password' ></label>
-				<input type='password' name='password2' id='Re-password' maxlength="50" placeholder="Re-enter your Password"/><br>
-				
+				<input type='password' name='password2' id='Re-password' maxlength="50" placeholder="Re-enter your Password"/>
+				<?php
+				if ($password2Error) {
+					showError();
+				}
+
+				if ($passwordMissMatch) {
+					echo "<span class=\"error\">The Passwords must match</span>" ;
+				}
+				?>
+				<br>
 				<label for='Fname' ></label>
-				<input type='text' name='Fname' id='Fname' maxlength="50" id="firstname" placeholder="Your First Name"/><br>
-				
+				<input type='text' name='Fname' id='Fname' maxlength="50" id="firstname" placeholder="Your First Name"/>
+				<?php
+				if ($firstnameError) {
+					showError();
+				}
+				?>
+				<br>
 				<label for='Lname' ></label>
-				<input type='text' name='Lname' id='Lname' maxlength="50" id="lastname" placeholder="Your Last Name" /><br><br>
-
+				<input type='text' name='Lname' id='Lname' maxlength="50" id="lastname" placeholder="Your Last Name" />
+				<?php
+				if ($lastnameError) {
+					showError();
+				}
+				?>
+				<br><br>
 				<input type='submit' name='submit' value='Sign-Up' id='signup' />
 
 			</fieldset>
