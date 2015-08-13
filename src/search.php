3a9ee7d39fabebaaf41aaa8378ed2b258be34da8
Bradley Imai <imaib@onid.oregonstate.edu>
2015-08-13 14:18:34 -0700

    I added a search page

---
+++ src/search.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Search</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="business_logic.js"></script>
+	</head>
+
+	<body>
+		<form id='search'>
+			<fieldset id='brad-search'>
+				<lengend>Search</lengend>
+
+				<input type='submit' name='submit' value='Home' id='home' />
+
+
+
+			</fieldset>
+		</form>
+	</body>
+</html>
