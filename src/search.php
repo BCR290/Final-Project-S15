05390ec4c4248a7f0c8f55ffe2928c66b2075f9e
Yipeng Song <songyip@flip2.engr.oregonstate.edu>
2015-08-14 16:39:44 -0700

    I made the button works

--- src/search.php
+++ src/search.php
@@ -18,8 +18,11 @@
 			<form action="logout.php" method="POST">
 				<br><input type='submit' name='submit' value='Log Out' class = 'buttonInPage' />
 			</form>
-			<input type='submit' name='submit' value='Home Page' class = 'buttonInPage' /><br><br><br>
 			
+			<form action="page.php">
+				<input type='submit' name='submit' value='Home Page' class = 'buttonInPage' href='page.php'/><br><br><br>
+			</form>
+
 			<fieldset id='search_page'>
 				<br><input type='text' name='searchBox' id='searchBox' placeholder = "Enter the key word here" />
 				<input type='submit' name='searchBtn' value='SEARCH' id = 'searchBtn'/><br><br>
